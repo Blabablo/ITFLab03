72131ec805244d466ba36054e749c0c2a2ba7191
Blabablo <74305907+Blabablo@users.noreply.github.com>
2020-11-19 20:18:37 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,7 +1,7 @@
 <?php
 $id=$_GET['ID'];
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'databaseitf.mysql.database.azure.com', 'superoof@databaseitf', 'Pin187932', 'itflab', 3306);
+mysqli_real_connect($conn, 'onlyforlab03.mysql.database.azure.com', 'imnottitan@onlyforlab03', 'Hahaha555', 'itflab', 3306);
 $res = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID='$id'");
 $row = mysqli_fetch_array($res)
 ?>
